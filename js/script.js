d762305788ec63c809a9e0f48cf0ea5ade5ede8b
kewinMarchand <kewin.marchand@gmail.com>
2018-07-27 11:22:33 +0200

    Initialisation Firebase et Vue
    Changez les paramètres Firebase par les votres:
    https://firebase.google.com/
    
    Doc de Vue.js:
    https://vuejs.org/

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Initialize Firebase
+var config = {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxxxxxx.firebaseio.com",
+    projectId: "xxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxxxxxx"
+};
+firebase.initializeApp(config);
+
+
+// Initialize Vue
+new Vue({
+    el: '#app',
+    data: {
+
+    }
+})
